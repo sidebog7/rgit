a347e42074e34a401315c1a369a5c17f81877b0f
Gareth Pendleton <gareth.sidebottom@gmail.com>
2019-04-15 19:45:37 +0100

    Changed inito to a subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 mod cmds;
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use cmds::init;
 
 fn main() {
@@ -8,13 +8,16 @@ fn main() {
         .version("0.1")
         .about("Rust implementation of Git")
         .author("Gareth Pendleton<gareth.sidebottom@gmail.com>")
-        .arg(Arg::with_name("init").help("Initialises rGit repository"))
-        .arg(Arg::with_name("path").help("File path").multiple(true))
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Initialises rGit repository")
+                .arg(Arg::with_name("path").help("File path").multiple(true)),
+        )
         .get_matches();
 
-    if matches.is_present("init") {
+    if let Some(sub_com) = matches.subcommand_matches("init") {
         let mut paths: Vec<&str> = Vec::new();
-        if let Some(path_params) = matches.values_of("path") {
+        if let Some(path_params) = sub_com.values_of("path") {
             for path in path_params {
                 paths.push(path);
             }
